6ecfa7b506933a539572c36dc4c6d037feff5668
prof <prof>
2020-11-04 12:15:30 +0100

    ejemplo mysql sencillo

--- ADConectores/src/principal/Principal.java
+++ ADConectores/src/principal/Principal.java
@@ -2,8 +2,15 @@ package principal;
 
 import java.util.Scanner;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import dbEmbebidaSqlite.Artista;
 import dbEmbebidaSqlite.OperacionesDb;
+import ejemploMySQL.Conexion;
+import java.sql.Connection;
+
 
 public class Principal {
 
@@ -16,7 +23,10 @@ public class Principal {
 //		
 //		a.verTodosArtistas();
 		
-		verCancionesAlbum();
+		//verCancionesAlbum();
+		verCiudadesPais();
+		
+		
 	}
 	
 	public static void verCancionesAlbum() {
@@ -29,6 +39,29 @@ public class Principal {
 		odb.listaCancionesDisco(titulo);
 		
 		
+	}
+	
+	
+	public static void verCiudadesPais() {
+		
+		try {
+		Conexion con = new Conexion();
+		Connection cx = con.conectar();
+		
+		PreparedStatement st = cx.prepareStatement("select * from city where countrycode like 'ESP'");
+		
+		ResultSet results = st.executeQuery();
+		while(results.next()) {
+			System.out.println("Ciudad: "+results.getString("Name"));
+			System.out.println("Región: "+results.getString("District"));
+		}
+		
+		
+		
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+				
 	}
 
 }
